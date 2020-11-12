af4067bd4d522ae4b5faacb918c0fd8b9a2cb11f
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-11-12 23:53:17 +0500

    add mssql server support for non-containerized env

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ const express = require('express')
 const { ApolloServer, gql } = require('apollo-server-express')
 const Sequelize = require('sequelize')
 
+if (
+  typeof process.env.CONTAINERIZED === 'string' &&
+  process.env.CONTAINERIZED.toLowerCase() === 'false'
+) {
+  require('dotenv').config()
+}
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOSTNAME,
-    dialect: 'mssql',
+    dialect: process.env.DB_PROVIDER,
   }
-)
-
-console.log(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.DB_HOSTNAME
 )
 
 sequelize
