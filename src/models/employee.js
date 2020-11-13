85f4de7113b16a265b5fa58dcbca53144ce7149a
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-11-13 08:59:52 +0500

    fix letter case for employee model

--- src/models/employee.js
+++ src/models/employee.js
@@ -9,62 +9,62 @@ module.exports = sequelize =>
         field: 'emp_no',
         primaryKey: true,
       },
-      // firstName: {
-      //   type: DataTypes.STRING,
-      //   field: 'Emp_firstname',
-      // },
-      // lastName: {
-      //   type: DataTypes.STRING,
-      //   field: 'Emp_lastname',
-      // },
-      // middleName: {
-      //   type: DataTypes.STRING,
-      //   field: 'Emp_middle',
-      // },
-      // designation: {
-      //   type: DataTypes.STRING,
-      //   field: 'Dsgn_no',
-      // },
-      // permanentAddress: {
-      //   type: DataTypes.STRING,
-      //   field: 'Per_address',
-      // },
-      // permanentCity: {
-      //   type: DataTypes.STRING,
-      //   field: 'Per_city',
-      // },
-      // currentAddress: {
-      //   type: DataTypes.STRING,
-      //   field: 'Pr_address',
-      // },
-      // currentCity: {
-      //   type: DataTypes.STRING,
-      //   field: 'Pr_city',
-      // },
-      // phone: {
-      //   type: DataTypes.STRING,
-      //   field: 'Res_telno',
-      // },
-      // email: {
-      //   type: DataTypes.STRING,
-      //   field: 'Emp_email',
-      // },
-      // joiningDate: {
-      //   type: DataTypes.DATE,
-      //   field: 'Joining_date',
-      // },
-      // resignDate: {
-      //   type: DataTypes.DATE,
-      //   field: 'Resign_date',
-      // },
-      // status: {
-      //   type: DataTypes.STRING,
-      //   field: 'Status',
-      // },
-      // nic: {
-      //   type: DataTypes.STRING,
-      //   field: 'NIC',
-      // },
+      firstName: {
+        type: DataTypes.STRING,
+        field: 'emp_firstname',
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        field: 'emp_lastname',
+      },
+      middleName: {
+        type: DataTypes.STRING,
+        field: 'emp_middle',
+      },
+      designation: {
+        type: DataTypes.STRING,
+        field: 'dsgn_no',
+      },
+      permanentAddress: {
+        type: DataTypes.STRING,
+        field: 'per_address',
+      },
+      permanentCity: {
+        type: DataTypes.STRING,
+        field: 'per_city',
+      },
+      currentAddress: {
+        type: DataTypes.STRING,
+        field: 'pr_address',
+      },
+      currentCity: {
+        type: DataTypes.STRING,
+        field: 'pr_city',
+      },
+      phone: {
+        type: DataTypes.STRING,
+        field: 'res_telno',
+      },
+      email: {
+        type: DataTypes.STRING,
+        field: 'emp_email',
+      },
+      joiningDate: {
+        type: DataTypes.DATE,
+        field: 'joining_date',
+      },
+      resignDate: {
+        type: DataTypes.DATE,
+        field: 'resign_date',
+      },
+      status: {
+        type: DataTypes.STRING,
+        field: 'status',
+      },
+      nic: {
+        type: DataTypes.STRING,
+        field: 'nic',
+      },
     },
     {
       tableName: 'employee',
