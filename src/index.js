b4ba00daa44a85c46fb264356da3456c5983575c
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-11-16 02:21:50 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const db = require('./database')
-
 const {
   server: { port },
 } = require('./config')
