8b68c3fc902594ce78523f7946a3b33bce1ce7de
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-12-29 22:35:22 +0500

    fix paths

--- tests/helper.js
+++ tests/helper.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server-express')
 const { createTestClient } = require('apollo-server-testing')
-const typeDefs = require('../src/typedefs')
-const resolvers = require('../src/resolvers')
+const typeDefs = require('../server/typedefs')
+const resolvers = require('../resolvers')
 
 const createTestServer = ctx => {
   const server = new ApolloServer({
