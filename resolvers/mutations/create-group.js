84429376e765f06ce325c38cace6d7d43951cd5b
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-02 04:57:07 +0500

    fix when name, image and descriptions are empty

--- resolvers/mutations/create-group.js
+++ resolvers/mutations/create-group.js
@@ -1,15 +1,22 @@
+const { UserInputError } = require('apollo-server-express')
 const { storeFile } = require('../../utils/storage')
 
 module.exports = async (_, { input }, { db, user }) => {
   const { name, description, image } = input
+
+  if (!name.trim().length || !description.trim().length || !image) {
+    throw new UserInputError(
+      `Please provide a valid name, image and description.`
+    )
+  }
 
   const { filename } = await storeFile(image)
 
   const User = await db.models.User.findOne({ where: { id: user.id } })
 
   const Group = await db.models.Group.create({
-    name,
-    description,
+    name: name.trim(),
+    description: description.trim(),
     image: filename,
     userId: User.id,
   })
