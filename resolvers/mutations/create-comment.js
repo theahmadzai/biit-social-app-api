0ceb8eaeca8028451c37f3a4234ab1ec79fb8182
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-02 01:32:24 +0500

    don't allow empty comments

--- resolvers/mutations/create-comment.js
+++ resolvers/mutations/create-comment.js
@@ -1,5 +1,11 @@
+const { UserInputError } = require('apollo-server-express')
+
 module.exports = async (_, { input }, { db, user }) => {
   const { content, postId } = input
+
+  if (!content.trim().length) {
+    throw new UserInputError('Write something before sending.')
+  }
 
   return await db.models.Comment.create({
     content,
