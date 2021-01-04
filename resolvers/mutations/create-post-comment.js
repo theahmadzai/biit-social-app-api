24c2307c6163db6b9ecde08104c327cbb903fb1b
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-05 02:05:36 +0500

    rename to create-post-comment

---
+++ resolvers/mutations/create-post-comment.js
@@ -0,0 +1,25 @@
+const { UserInputError } = require('apollo-server-express')
+const pubSub = require('../pubsub')
+const { notifications } = require('../events')
+
+const { NEW_NOTIFICATION } = notifications
+
+module.exports = async (_, { input }, { db, user }) => {
+  const { content, postId } = input
+
+  if (!content.trim().length) {
+    throw new UserInputError('Empty comment.')
+  }
+
+  const notification = {
+    title: `User: '${user.id}' commented on post: '${postId}'`,
+  }
+
+  pubSub.publish(NEW_NOTIFICATION, { notification })
+
+  return await db.models.Comment.create({
+    content,
+    postId,
+    userId: user.id,
+  })
+}
