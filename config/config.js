6f64a31e37b6dd5193eac8c614d26f969c1345cb
Javed Ahmadzai <theahmadzai@hotmail.com>
2021-01-23 08:54:55 +0500

    fix db env configs

--- config/config.js
+++ config/config.js
@@ -5,21 +5,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    host: 'localhost',
     dialect: process.env.DB_DIALECT,
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
   },
 }
