d506b69a117704325515f620704898fd9dc4cf84
Muhammad Javed <theahmadzai@hotmail.com>
2021-02-03 03:04:19 +0500

    use dbatabase_url instead

--- config/config.js
+++ config/config.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: 'localhost',
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: 'DATABASE_URL',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
   },
 }
--- config/index.js
+++ config/index.js
@@ -10,12 +10,7 @@ exports.app = {
 }
 
 exports.db = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL || null,
 }
 
 exports.auth = {
